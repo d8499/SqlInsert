005c662fc4827ddaa4630150181daba130f0d3b5
COE-028 <coe-028@coe-028s-imac.local>
2020-01-09 13:47:29 +0530

    Initial Commit

---
+++ SQLsimple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SQLsimple
+//
+//  Created by COE-028 on 09/01/20.
+//  Copyright © 2020 COE-028. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
